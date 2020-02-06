9a76af038726c250957875223c0ba983278bc5dd
Gergő Gazda <info@gazdagergo.com>
2020-02-06 04:32:47 +0100

    use send

--- parse.js
+++ parse.js
@@ -9,9 +9,19 @@ var options = {
   args: ['./htmls/M01_T002_S021.html', './tsv/utca.tsv']
 };
 
-export default () => PythonShell.run('getUtca.py', options, function (err, results) {
+export default () => {
+let pyShell = new PythonShell('getUtca.py', options)
+
+pyShell.on('message', message => {
+  console.log(message)
+})
+
+pyShell.send('./htmls/M01_T002_S021.html')
+}
+
+/* export default () => PythonShell.run('getUtca.py', options, function (err, results) {
   if (err) 
     throw err;
   // Results is an array consisting of messages collected during execution
   console.log(results);
-});
+}); */
--- pythonScripts/getUtca.py
+++ pythonScripts/getUtca.py
@@ -6,53 +6,36 @@ from bs4 import BeautifulSoup
 import sys
 
 
-def main():
-		""" Main entry point of the app """
+def main(inputFile):
+	""" Main entry point of the app """
+	
+	soup = BeautifulSoup(open(inputFile), "html.parser")
 
-		inputFile = sys.argv[1]
-		outputFile = sys.argv[2]
-		# parser = etree.HTMLParser()
-		# tree = etree.parse(inputFile, parser)
-		
-		soup = BeautifulSoup(open(inputFile), "html.parser")
+	utcaList = soup.findAll("div", {"class": "nvi-search-list"})[0] \
+	.findAll("div", {"class": "nvi-custom-table"})[0] \
+	.findAll("table", {"class": "table"})[0] \
+	.findAll("tr")
 
-		#utcaList = tree.xpath("//*[contains(@class, 'list-item row-fluid')]")
-		# '//*[@id="_onkszavazokorieredmenyek_WAR_nvinvrportlet_fm"]/div/div[3]/div/table/tr[1]/td/div/div[1]/span/text()'
+	def getUtcaNev(tRow):
+		utcanev = tRow.findAll("div", {"class": "span6"})[0] \
+		.getText() 
 
-		utcaList = soup.findAll("div", {"class": "nvi-search-list"})[0] \
-		.findAll("div", {"class": "nvi-custom-table"})[0] \
-		.findAll("table", {"class": "table"})[0] \
-		.findAll("tr")
+		return utcanev
 
-		def getUtcaNev(tRow):
-			utcanev = tRow.findAll("div", {"class": "span6"})[0] \
-			.getText() #\
-			#.encode('utf-8').strip()
+	def getHazszamok(tRow):
+		hazszamok = tRow.findAll("div", {"class": "span6"})[1] \
+		.getText()
 
-			return utcanev
+		return hazszamok
 
-		def getHazszamok(tRow):
-			hazszamok = tRow.findAll("div", {"class": "span6"})[1] \
-			.getText() #\
-			#.encode('utf-8').strip()
+	for item in utcaList:
+		szkUtca = getUtcaNev(item).replace('Cím::  ', '')
+		szkHazszamok = getHazszamok(item).replace('Tartomány típusa::  ', '')
+		print(szkUtca.strip())
+		print(szkHazszamok.strip())
 
-			return hazszamok
+for line in sys.stdin:
+	main(line.replace('\n', ''))
 
-		for item in utcaList:
-			szkUtca = getUtcaNev(item).replace('Cím::  ', '')
-			szkHazszamok = getHazszamok(item).replace('Tartomány típusa::  ', '')
-			print(szkUtca.strip())
-			print(szkHazszamok.strip())
-
-""" 			with open(outputFile,'a') as fd:
-				fd.write("\n")
-				fd.write(szkUtca)
-				fd.write("\t")
-				fd.write(szkHazszamok)
-				fd.write("\t")
-				fd.write(inputFile.replace('.html', '').split('/')[-1]) """
-
-
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
+""" if __name__ == "__main__":
+	main()	 """
