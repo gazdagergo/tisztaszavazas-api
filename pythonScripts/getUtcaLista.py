78a30182893ca176e1490450c7a0158ad3601886
Gergő Gazda <info@gazdagergo.com>
2020-03-09 21:02:47 +0100

    parse area with regex

--- pythonScripts/getUtcaLista.py
+++ pythonScripts/getUtcaLista.py
@@ -1,3 +1,24 @@
+# coding=utf-8
+#!/usr/bin/env python3
+
+import re
+
+regex = r"(?:(^[^\d]+)(\d+|(?:(\d+)-\d+))(?:\s?)([A-z])?(?:\s-\s)((?:[\d\s]+)|(?:\d+-(\d+)))(?:\s?)([A-z]?)$)|^.+$"
+
+def emptyOrStrip(string):
+	return "" if not string else string.strip()
+
+def isInt(value):
+  try:
+    int(value)
+    return True
+  except ValueError:
+    return False
+
+def nullOrInt(string):
+	result = float(string.replace(' ', '')) if string and isInt(string.replace(' ', '')) else None
+	return 9999 if (result and (result > 9999)) else result
+
 def getUtcaLista(soup):
 	completeSettlement = soup.findAll("div", {"class": "nvi-complete-settlement-wrapper"})
 
@@ -29,10 +50,21 @@ def getUtcaLista(soup):
 	for item in utcaList:
 		szkUtca = getUtcaNev(item).replace('Cím::  ', '')
 		szkHazszamok = getHazszamok(item).replace('Tartomány típusa::  ', '')
+
+		matches = re.match(regex, szkUtca)
+
+		kozteruletNev = emptyOrStrip(matches.group(1)) if emptyOrStrip(matches.group(1)) else emptyOrStrip(matches.group(0))
+		vegsoHazszam = nullOrInt(matches.group(6)) if nullOrInt(matches.group(6)) else nullOrInt(matches.group(5))
+
 		utcaListPairs.append({
-			"szkUtca": szkUtca.strip(),
-			"szkHazszamok": szkHazszamok.strip()
+			"utcaString": matches.group().strip(),
+			"kozteruletNev": kozteruletNev,
+			"kezdoHazszam": nullOrInt(matches.group(2)),
+			"vegsoHazszam": vegsoHazszam,
+			"megjegyzes": szkHazszamok.strip()
 		})
+		
+# https://regex101.com/r/vahPd7/1
 
 	return {
 		"kozteruletek": utcaListPairs,
