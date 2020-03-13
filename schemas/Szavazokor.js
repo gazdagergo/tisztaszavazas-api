a9440e3e109bdce018509840b291afd6a3ab3616
Gergő Gazda <info@gazdagergo.com>
2020-03-13 16:32:06 +0100

    schema: add szavkor cim and kozteruletek

--- schemas/Szavazokor.js
+++ schemas/Szavazokor.js
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 
 const SzavazokorSchema = mongoose.Schema({
-  kozteruletek: Array,
   egySzavazokorosTelepules: Boolean,
   szavkorSorszam: Number,
+  szavkorCim: String,
   kozigEgyseg: {
     type: Object,
     ref: 'KozigEgyseg'
-  }
+  },
+  kozteruletek: [{
+    type: Object,
+    ref: 'Kozterulet'
+  }]
 })
 
 export default mongoose.model('Szavazokor', SzavazokorSchema);
