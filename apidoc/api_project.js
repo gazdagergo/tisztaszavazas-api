5d21e46c131bc164e04463aff86d27bd1af7c853
Gergő Gazda <info@gazdagergo.com>
2020-03-13 16:32:27 +0100

    add example html

---
+++ apidoc/api_project.js
@@ -0,0 +1,21 @@
+define({
+  "name": "Tisztaszavazás API dokumentáció",
+  "version": "0.1.0",
+  "description": "Az api a szavazókörök publikus adatait tartalmazza. API kulcsot a info@tisztaszavazas.hu email címen lehet igényelni.",
+  "title": "api.tisztaszavazas.hu",
+  "url": "https://api.tisztaszavazas.hu",
+  "sampleUrl": "https://api.tisztaszavazas.hu",
+  "template": {
+    "withCompare": false,
+    "withGenerator": false,
+    "aloneDisplay": false
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-04-07T14:28:33.498Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
