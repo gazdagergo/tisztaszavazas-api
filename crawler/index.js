438aebcc6f300512566c13b9cf56ede1467a3db0
Gergő Gazda <info@gazdagergo.com>
2020-03-23 08:48:08 +0100

    crawl: break if not running

--- crawler/index.js
+++ crawler/index.js
@@ -10,11 +10,12 @@ export const crawl = async (szavazokorok, query) => {
 		const { 0: { _id: processId } } = await Process.insertMany([{ isRunning: true }])
 		for (({ _id: szavazokorId }) of szavazokorok) {
 			const { isRunning } = await Process.findById(processId);
-			if (isRunning) {
-				await sleep(randomInt(6000, 11000))
-				const [code, { message }] = await scraper_GET(szavazokorId, query)
-				console.log(code, message, szavazokorId )
-			}
+			if (!isRunning) break;
+
+			await sleep(randomInt(6000, 11000))
+			const [code, { message }] = await scraper_GET(szavazokorId, query)
+			console.log(code, message, szavazokorId )
+
 		}
 		console.log('crawling done')
 	} catch(error){
