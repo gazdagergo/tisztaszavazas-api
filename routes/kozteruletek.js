ed58596de656990a3fcd95f7d391c100018148f7
Gergő Gazda <info@gazdagergo.com>
2020-04-04 21:28:17 +0200

    introduce kozigegysegek

--- routes/kozteruletek.js
+++ routes/kozteruletek.js
@@ -1,44 +1,18 @@
 import express from 'express';
 import Kozterulet from '../schemas/Kozterulet';
 import parseQuery from '../functions/parseQuery';
-
-const DEFAULT_LIMIT = 9999999;
+import Szavazokor from '../schemas/Szavazokor';
 
 const router = express.Router()
 
-router.get('/:KozteruletId?', async (req, res) => {
-  let {
-    params: { KozteruletId },
-    query: { limit = DEFAULT_LIMIT, ...query }
-  } = req;
-
-  try {
-    let result;
-    if (KozteruletId) {
-      result = await Kozterulet.findById(KozteruletId)
-    } else {
-      query = parseQuery(query)
-      console.log(query)
-      result = await Kozterulet.find(query).limit(+limit)
-    }
-    res.status(result ? 200 : 400)
-    res.json(result || 'Kozterulet not found')
-  } catch(error) {
-    res.json({ error: error.message })
-  }
+router.get('/', async (req, res) => {
+  const result = await Szavazokor.aggregate([
+    { $match: { 'kozigEgyseg.kozigEgysegNeve': 'Balatonfenyves' } },
+    { $unwind: '$kozteruletek' },
+    { $project: { _id: 0, kozteruletek: 1 } }
+  ])
+  res.json(result)
 })
 
-router.post('/', async (req, res) => {
-  let { body } = req;
-
-  body = Array.isArray(body) ? body : [ body ];
-
-  try {
-    const insertedRecords = await Kozterulet.insertMany(body)
-    res.json(insertedRecords)
-  } catch(error){
-    res.json({ error: error.message })
-  }
-})
 
 export default router;
--- schemas/KozigEgyseg.js
+++ schemas/KozigEgyseg.js
@@ -5,11 +5,13 @@ const KozigEgysegSchema = mongoose.Schema({
   megyeKod: Number,
   telepulesKod: Number,
   kozigEgysegNeve: String,
+  kozteruletek: Array,
+  egySzavazokorosTelepules: Boolean,
   tipus: String,
   tipusKod: {
     type: String,
     enum: ['ME', 'FV', 'MV', 'BK', 'TF', 'TA',]
-  }
+  },  
 })
 
 export default mongoose.model('KozigEgyseg', KozigEgysegSchema);
