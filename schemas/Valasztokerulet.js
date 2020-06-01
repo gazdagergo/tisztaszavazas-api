7ad4264b567d8759b8ec5247e161cf25fbf1f1bd
Gergő Gazda <info@gazdagergo.com>
2020-06-01 10:24:17 +0200

    update szk schemas

--- schemas/Valasztokerulet.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const ValasztokeruletSchema = mongoose.Schema({
-	"tipus": String,
-	"leiras": String,
-	"szam": Number
-})
-
-export default mongoose.model('Valasztokerulet', ValasztokeruletSchema);
