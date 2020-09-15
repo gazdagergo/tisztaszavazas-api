966d730ad1fe808a2cf0e76e0c4a934c9fe84df1
Gergő Gazda <info@gazdagergo.com>
2020-09-15 22:45:43 +0200

    update schemas with borsod 6

--- schemas/KozigEgyseg.js
+++ schemas/KozigEgyseg.js
@@ -11,5 +11,6 @@ export const onk2019_v1_kozigegyseg = model('onk2019_v1_kozigegyseg', KozigEgyse
 export const onk2019_v2_kozigegyseg = model('onk2019_v2_kozigegyseg', KozigEgysegSchema);
 export const ogy2018_v1_kozigegyseg = model('ogy2018_v1_kozigegyseg', KozigEgysegSchema);
 export const ogy2018_v2_kozigegyseg = model('ogy2018_v2_kozigegyseg', KozigEgysegSchema);
+export const idbo620_v1_kozigegyseg = model('idbo620_v1_kozigegyseg', KozigEgysegSchema);
 
 export default KozigEgysegSchema
--- schemas/index.js
+++ schemas/index.js
@@ -1,8 +1,17 @@
 import {
+	onk2019_v1_szavazokor_url,
+	onk2019_v2_szavazokor_url,
+	ogy2018_v1_szavazokor_url,
+	ogy2018_v2_szavazokor_url,
+	idbo620_v1_szavazokor_url,
+} from "./SzavazokorUrl"
+
+import {
 	onk2019_v1_szavazokor,
 	onk2019_v2_szavazokor,
 	ogy2018_v1_szavazokor,
 	ogy2018_v2_szavazokor,
+	idbo620_v1_szavazokor,
 } from "./Szavazokor"
 
 import {
@@ -10,9 +19,34 @@ import {
 	onk2019_v2_kozigegyseg,
 	ogy2018_v1_kozigegyseg,
 	ogy2018_v2_kozigegyseg,
+	idbo620_v1_kozigegyseg,
 } from "./KozigEgyseg"
 
+import {
+	onk2019_v1_valasztokerulet,
+	onk2019_v2_valasztokerulet,
+	ogy2018_v1_valasztokerulet,
+	ogy2018_v2_valasztokerulet,
+	idbo620_v1_valasztokerulet,
+} from "./Valasztokerulet"
+
 export default {
+	SzavazokorUrl: {
+		onk2019: {
+			v1: onk2019_v1_szavazokor_url,
+			v2: onk2019_v2_szavazokor_url,
+			latest: onk2019_v2_szavazokor
+		},
+		ogy2018: {
+			v1: ogy2018_v1_szavazokor_url,
+			v2: ogy2018_v2_szavazokor_url,
+			latest: ogy2018_v1_szavazokor
+		},
+		idbo620: {
+			v1: idbo620_v1_szavazokor_url,
+			latest: idbo620_v1_szavazokor_url,
+		}
+	},
 	Szavazokor: {
 		onk2019: {
 			v1: onk2019_v1_szavazokor,
@@ -22,19 +56,43 @@ export default {
 		ogy2018: {
 			v1: ogy2018_v1_szavazokor,
 			v2: ogy2018_v2_szavazokor,
-			latest: ogy2018_v2_szavazokor
-		}
+			latest: ogy2018_v2_szavazokor,
+		},
+		idbo620: {
+			v1: idbo620_v1_szavazokor,
+			latest: idbo620_v1_szavazokor,
+		},		
 	},
 	KozigEgyseg: {
 		onk2019: {
 			v1: onk2019_v1_kozigegyseg,
-			v1: onk2019_v2_kozigegyseg,
+			v2: onk2019_v2_kozigegyseg,
 			latest: onk2019_v2_kozigegyseg,
 		},
 		ogy2018: {
 			v1: ogy2018_v1_kozigegyseg,
 			v2: ogy2018_v2_kozigegyseg,
 			latest: ogy2018_v2_kozigegyseg,
-		}
+		},
+		idbo620: {
+			v1: idbo620_v1_kozigegyseg,
+			latest: idbo620_v1_kozigegyseg,
+		},
+	},
+	Valasztokerulet: {
+		onk2019: {
+			v1: onk2019_v1_valasztokerulet,
+			v2: onk2019_v2_valasztokerulet,
+			latest: onk2019_v2_valasztokerulet
+		},
+		ogy2018: {
+			v1: ogy2018_v1_valasztokerulet,
+			v2: ogy2018_v2_valasztokerulet,
+			latest: ogy2018_v2_valasztokerulet
+		},
+		idbo620: {
+			v1: idbo620_v1_valasztokerulet,
+			latest: idbo620_v1_valasztokerulet,
+		},		
 	}
 }
