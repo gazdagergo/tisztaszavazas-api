564eb7676e6b811516361d96f2e3b097bd6b96f7
Gergő Gazda <info@gazdagergo.com>
2021-01-12 13:53:22 +0100

    feat(vk): add route

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import path from 'path';
@@ -8,6 +7,8 @@ import szkRoute from './routes/szavazokorok';
 import kozigEgysegRoute from './routes/kozigegysegek';
 import vhuPageRoute from './routes/vhupage';
 import usageRoute from './routes/usage';
+import valasztokeruletRoute from './routes/valasztokeruletek'
+import cors from 'cors';
 
 dotenv.config()
 const app =  express();
@@ -20,14 +21,14 @@ const corsOptions = {
 
 // Middlewares
 app.use(bodyParser.json({limit: '50mb'}))
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 app.use('/', express.static(path.join(__dirname, 'apidoc')))
 app.use('/kozigegysegek', kozigEgysegRoute)
 app.use('/szavazokorok', szkRoute)
 app.use('/usage', usageRoute)
 app.use('/vhupage', vhuPageRoute)
-
+app.use('/valasztokeruletek', valasztokeruletRoute)
 
 // Connect to DB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -36,7 +37,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 () => console.log('connected to DB'))
 
 // Listen
-var port = process.env.PORT || 1338;
+var port = process.env.PORT || 1338
 app.listen(port, () => {
   console.log(`listening on ${port}`)
 })
