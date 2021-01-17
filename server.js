4038f2b767538c6d6bec190d0b18499f525be154
Gergő Gazda <info@gazdagergo.com>
2021-01-17 16:42:35 +0100

    copy zipapi server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const path = require('path')
+const connectDb = require('./functions/connectDb')
+
 const szkRoute = require('./routes/szavazokorok')
 const kozigEgysegRoute = require('./routes/kozigegysegek')
 const vhuPageRoute = require('./routes/vhupage')
@@ -11,8 +12,11 @@ const usageRoute = require('./routes/usage')
 const valasztokeruletRoute = require('./routes/valasztokeruletek')
 const eredmenyekRoute = require('./routes/eredmenyek')
 
+
 dotenv.config()
-const app =  express();
+const app =  express()
+
+connectDb()
 
 const corsOptions = {
   origin: '*',
@@ -30,12 +34,7 @@ app.use('/szavazokorok', szkRoute)
 app.use('/usage', usageRoute)
 app.use('/vhupage', vhuPageRoute)
 app.use('/valasztokeruletek', valasztokeruletRoute)
-
-// Connect to DB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true
-},
-() => console.log('connected to DB'))
+app.use('/eredmenyek', eredmenyekRoute)
 
 // Listen
 var port = process.env.PORT || 1338
