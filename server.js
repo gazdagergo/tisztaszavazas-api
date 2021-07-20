99ac3f47be045ad2302da0e5923d9ff0b2306cca
gazdagergo <info@gazdagergo.com>
2021-07-20 07:06:48 +0200

    refact: to scraper

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import path from 'path';
 import szkRoute from './routes/szavazokorok';
-import scrapeRoute from './routes/scrape';
 import kozigEgysegRoute from './routes/kozigegysegek-aggr';
-import sourceHtmlRoute from './routes/sourcehtmls';
-import urlsRoute from './routes/urls';
 import vhuPageRoute from './routes/vhupage';
 import usageRoute from './routes/usage';
 
@@ -25,14 +22,10 @@ app.use(bodyParser.json({limit: '50mb'}))
 app.use(cors(corsOptions));
 
 app.use('/', express.static(path.join(__dirname, 'apidoc')))
-app.use('/vhupage', vhuPageRoute)
-
-app.use('/szavazokorok', szkRoute)
-app.use('/scrape', scrapeRoute)
 app.use('/kozigegysegek', kozigEgysegRoute)
-app.use('/sourcehtmls', sourceHtmlRoute)
-app.use('/urls', urlsRoute)
+app.use('/szavazokorok', szkRoute)
 app.use('/usage', usageRoute)
+app.use('/vhupage', vhuPageRoute)
 
 
 // Connect to DB
