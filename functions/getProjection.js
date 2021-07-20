e3a2eac6fcb0515421d74ca232160725543ff9a3
gazdagergo <info@gazdagergo.com>
2021-07-20 07:06:45 +0200

    szk: refact to functions

---
+++ functions/getProjection.js
@@ -0,0 +1,59 @@
+export default ({ roles }, context) => {
+  const isAdmin = roles && roles.includes('admin')
+
+  let projection = {
+    sourceHtmlUpdated: 0,
+    parsedFromSrcHtml: 0,
+    createdAt: 0,
+    vhuUrl: 0,
+    'kozigEgyseg.megyeKod': 0,
+    'kozigEgyseg.telepulesKod': 0,
+    polygonUrl: 0,
+    valasztasAzonosito: 0,
+    helyadatok: 0
+  }
+
+  switch (context) {
+    case 'withQuery':
+    case 'noQuery': return ({
+      kozteruletek: 0,
+      sourceHtmlUpdated: 0,
+      frissitveValasztasHun: 0,
+      parsedFromSrcHtml: 0,
+      vhuUrl: 0,
+      polygonUrl: 0,
+      createdAt: 0,
+      updatedAt: 0,
+      egySzavazokorosTelepules: 0,
+      'kozigEgyseg.megyeKod': 0,
+      'kozigEgyseg.telepulesKod': 0,
+      valasztasAzonosito: 0,
+      helyadatok: 0
+    })
+
+    case 'filterStreet': return ({
+      szavazokorSzama: 1,
+      'kozigEgyseg.kozigEgysegNeve': 1,
+      'kozigEgyseg.megyeNeve': 1
+    })
+
+    case 'withRegex': return ({
+      ...projection,
+      frissitveValasztasHun: 0,
+      szavazokorCime: 0,
+      valasztokSzama: 0,
+      valasztokerulet: 0,
+      akadalymentes: 0,
+      updatedAt: 0,
+      egySzavazokorosTelepules: 0,
+    })
+
+    default:
+      if (isAdmin) {
+        delete projection['kozigEgyseg.megyeKod']
+        delete projection['kozigEgyseg.telepulesKod']
+        delete projection.polygonUrl
+      }
+      return projection
+  }
+}
---
+++ functions/getSzkAggregationFilter.js
@@ -0,0 +1,27 @@
+export default query => {
+	let [_, filterCond] = Object.entries(query).reduce(
+		(acc, [key, value]) => {
+			if (key.includes('kozteruletek')){
+				return [ acc[0], { ...acc[1], [key]: value } ]
+			}
+			return [ {...acc[0], [key]: value }, acc[1] ]
+		},
+		[{}, {}] 
+	)
+
+	let regexStreetToFilter = '';
+
+	filterCond = Object.entries(filterCond).reduce((acc = [], [key, value]) => {
+		if (instanceOf(value, 'Object')) {
+			const [operator, value2] = Object.entries(value)[0]
+			return [...acc, { [operator]: [ `$$${key}`, value2 ]  }]
+		}
+		if (instanceOf(value, 'RegExp')) {
+			regexStreetToFilter = value
+			return acc
+		}
+		return [...acc, { $eq: [ `$$${key}`, value ]  }]
+	}, [])
+
+	return [filterCond, regexStreetToFilter]
+}
