5d908c97dc60fdff0433c784d5a5ba6ccc53291c
gazdagergo <info@gazdagergo.com>
2021-07-20 07:06:40 +0200

    py fix szklist duplication

--- pythonScripts/getSzkList.py
+++ pythonScripts/getSzkList.py
@@ -5,13 +5,7 @@ def getSzkList(soup):
 	szkList = soup.findAll("div", {"class": "nvi-search-list"})[0] \
 	.findAll("div", {"class": "nvi-custom-table"})[0] \
 	.findAll("table", {"class": "table"})[0] \
-	.findAll("tr")
-
-	# return szkList
-
-	# return {
-	# 	"foo": "bar"
-	# }
+	.findAll("tr", {"class": "hidden-desktop"})
 
 	def getSzavkorUrl(tRow):
 		szavkorHref = tRow.findAll("a", href=True)
