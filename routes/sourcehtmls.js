4a3a4f65ef855cef1708facb0c1431c18bab9cff
gazdagergo <info@gazdagergo.com>
2021-07-20 07:06:39 +0200

    sourcehtml endpoint

---
+++ routes/sourcehtmls.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import SourceHtml from '../schemas/SourceHtml';
+import parseQuery from '../functions/parseQuery';
+
+const DEFAULT_LIMIT = 10;
+
+const router = express.Router()
+
+router.get('/:SourceHtmlId?', async (req, res) => {
+  let {
+    params: { SourceHtmlId },
+    query: { limit = DEFAULT_LIMIT, ...query }
+  } = req;
+
+  try {
+    let result;
+    if (SourceHtmlId) {
+      result = await SourceHtml.findById(SourceHtmlId)
+    } else {
+      query = parseQuery(query)
+      console.log(query)
+      result = await SourceHtml.find(query, { html: 0 }).limit(+limit)
+    }
+    res.status(result ? 200 : 400)
+    res.json(result || 'SourceHtml not found')
+  } catch(error) {
+    res.json({ error: error.message })
+  }
+})
+
+export default router;
