02a0d92f6e68ce650f06cd29a8df0390debfb38a
gazdagergo <info@gazdagergo.com>
2021-07-20 07:06:41 +0200

    crawler: introduce initszk

---
+++ crawler/initSzks.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import SzavazokorUrl from "../schemas/SzavazokorUrl";
+import generateVhuUrl, { getUrlParams } from '../functions/generateVhuUrl';
+import Szavazokor from '../schemas/Szavazokor';
+
+dotenv.config()
+
+const valasztasAzonosito = process.argv[2] || 'onk2019';
+
+;(async () => {
+	await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+	let { preId } = getUrlParams(valasztasAzonosito);
+	const urlEntries = await SzavazokorUrl.find();
+	const toInsert = urlEntries.map(({
+		url: vhuUrl,
+		query
+	}) => {
+		const telepulesKod = +query[`_${preId}_telepulesKod`]
+		const megyeKod = +query[`_${preId}_megyeKod`]
+		const szavazokorSzama = +query[`_${preId}_szavkorSorszam`]
+
+		const polygonUrl = generateVhuUrl({
+			valasztasAzonosito,
+			'kozigEgyseg.megyeKod': megyeKod,
+			'kozigEgyseg.telepulesKod': telepulesKod,
+			szavazokorSzama,
+			context: 'polygon'
+		})
+
+		return {
+			szavazokorSzama,
+			kozigEgyseg: {
+				megyeKod,
+				telepulesKod
+			},
+			vhuUrl,
+			polygonUrl,
+			valasztasAzonosito
+		}
+	})
+
+	const response = await Szavazokor.insertMany(toInsert)
+	console.log(response)
+
+})()
