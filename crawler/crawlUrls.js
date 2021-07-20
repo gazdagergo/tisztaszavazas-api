6ad65df3e396c7c914400eef36f234a4cfb3e543
gazdagergo <info@gazdagergo.com>
2021-07-20 07:06:40 +0200

    crawl szk urls loop country

--- crawler/crawlUrls.js
+++ crawler/crawlUrls.js
@@ -1,19 +1,51 @@
 import axios from "axios";
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import url from 'url'
+import SzavazokorUrl from "../schemas/SzavazokorUrl";
 import generateVhuUrl from "../functions/generateVhuUrl";
 import parse from "../routes/scrape/parse";
+import { sleep } from ".";
 
-(async () => {
-	const query = {
-		context: 'szavazokorok',
-		'kozigEgyseg.megyeKod': 2,
-		page: 1
+dotenv.config()
+
+const MAX_MEGYEKOD = 20
+
+;(async () => {
+
+	await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+
+	for (let mk = 1; mk <= MAX_MEGYEKOD; mk+=1 ) {
+		for (let i = 1; i < 9999; i+=1) {
+			const query = {
+				context: 'szavazokorok',
+				'kozigEgyseg.megyeKod': mk,
+				page: i
+			}
+
+			const szkListUrl = generateVhuUrl(query)
+			console.log('------', mk, i)
+			try {
+				const { data: html } = await axios.get(szkListUrl);
+				const parsed = await parse(html, 'parseSzkList.py')
+				await sleep(1500)
+				if (parsed.error === 'NoResult') {
+					console.log('done')
+					break;
+				}
+ 				const urlArray = parsed.szkParams.map(({ szkUrl }) => ({
+					url: szkUrl,
+					query: szkUrl && url.parse(szkUrl, true)['query']
+				}))
+				.filter(({ url }) => url)
+
+				const response = await SzavazokorUrl.insertMany(urlArray)
+				console.log(response)		
+			} catch(error){
+				console.log(error)
+				console.log('done, error')
+				break;			
+			}
+		}
 	}
-
-	const szkListUrl = generateVhuUrl(query)
-
-	const { data: html } = await axios.get(szkListUrl);
-
-	const parsed = await parse(html, 'parseSzkList.py')
-
-	console.log(parsed)
 })()
