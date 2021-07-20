ab3ebf97f28e15b75b7975ade0ff4710c6a9eed9
gazdagergo <info@gazdagergo.com>
2021-07-20 07:06:45 +0200

    handle incorrect valasztas kod

--- routes/szavazokorok.js
+++ routes/szavazokorok.js
@@ -146,9 +146,14 @@ router.all('*', authorization)
 
 let Szavazokor;
 
-router.all('*', (req, _res, next) => { 
+router.all('*', (req, res, next) => { 
   const db = req.headers['x-valasztas-kodja'] || 'onk2019'
   Szavazokor = SzavazokorSchemas[`Szavazokor_${db}`]
+  if (!Szavazokor){
+    res.status(400)
+    res.json({'error': `Hibás választás kód: '${db}'` })
+    return
+  }
   next()
 })
 
